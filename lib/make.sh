5264a2065795ec20d3d0a27b64139c48797e804c
victore <victelk@gmail.com>
2018-02-08 15:15:13 +0200

    Correct make.sh file according to tensorflow issues 12482 and 13607.

--- lib/make.sh
+++ lib/make.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 CUDA_PATH=/usr/local/cuda/
 CXXFLAGS=''
@@ -11,15 +13,18 @@ cd roi_pooling_layer
 
 if [ -d "$CUDA_PATH" ]; then
 	nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
-		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
+		-I $TF_INC -I /home/victore/tensorflow/lib/python3.5/site-packages/tensorflow/include/external/nsync/public/ \
+		-D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
 		-arch=sm_37
 
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
-		roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
-		-lcudart -L $CUDA_PATH/lib64
+		roi_pooling_op.cu.o -I $TF_INC -I /home/victore/tensorflow/lib/python3.5/site-packages/tensorflow/include/external/nsync/public/ \
+		 -D GOOGLE_CUDA=1 -D _GLIBCXX_USE_CXX11_ABI=0 -fPIC $CXXFLAGS \
+		-lcudart -L $CUDA_PATH/lib64 -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -L$TF_LIB -ltensorflow_framework
 else
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
-		-I $TF_INC -fPIC $CXXFLAGS
+		-I $TF_INC -I /home/victore/tensorflow/lib/python3.5/site-packages/tensorflow/include/external/nsync/public/ \
+		-fPIC $CXXFLAGS
 fi
 
 cd ..
